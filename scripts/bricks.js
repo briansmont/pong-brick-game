373eda77e2f042990583822765b1314702320f14
briansmont <briansmont830@gmail.com>
2017-02-09 11:46:14 -0500

    bricks status changes to 0 upon collision, score displays and is incremented, when bricks are all gone alert that you have won

--- scripts/bricks.js
+++ scripts/bricks.js
@@ -37,6 +37,7 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+var score = 0;
 
 var bricks = [];
 for (c = 0; c < brickColumnCount; c++) {
@@ -87,10 +88,22 @@ function collisionDetection() {
 			if(b.status ==1) {
 				if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
 					dy = -dy;
+					b.status = 0;
+					score ++;
+					if (score == brickRowCount * brickColumnCount) {
+						alert("You have defeated the brickvaders!");
+						document.location.reload();
+					}
 				}
 			}
 		}
 	}
+}
+
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: " + score, 8, 20);
 }
 
 function draw() {
@@ -98,6 +111,7 @@ function draw() {
  drawBricks();
  drawBall();
  drawPaddle();
+ drawScore();
  collisionDetection();
 	
 // redirect ball
